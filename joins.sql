1ef50555d48b9a4293b508d5770d392e627bd32c
Tyler Nichols <tylernchls@gmail.com>
2017-01-14 19:20:42 -1000

    completed querys up to #10

--- joins.sql
+++ joins.sql
@@ -30,6 +30,30 @@ select title, content, users.username
     inner join posts on posts.users_id = users.id
       where users.created_at < 'January 1, 2015';
 
+-- query 7
+select comments.*, posts.title as post_title
+  from comments
+    inner join posts on comments.posts_id = posts.id;
+
+-- query 8?
+select posts.title as post_title, posts.url as post_url , comments.body as comment_body
+  from posts
+    inner join comments on comments.posts_id = posts.id
+      where posts.created_at < 'January 1, 2015';
+
+-- query 9
+select posts.title as post_title, posts.url as post_url , comments.body as comment_body
+  from posts
+    inner join comments on comments.posts_id = posts.id
+      where posts.created_at > 'January 1, 2015';
+
+-- query 10
+select posts.title as post_title, posts.url as post_url , comments.body as comment_body
+  from posts
+    inner join comments on comments.posts_id = posts.id
+      where comments.body  = 'USB';
+
+
 
 
 
