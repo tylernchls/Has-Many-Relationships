9c4a738446b71867b59713e08cfb42c2bce77e09
Tyler Nichols <tylernchls@gmail.com>
2017-01-14 20:19:21 -1000

    finished all queries

--- joins.sql
+++ joins.sql
@@ -51,9 +51,28 @@ select posts.title as post_title, posts.url as post_url , comments.body as comme
 select posts.title as post_title, posts.url as post_url , comments.body as comment_body
   from posts
     inner join comments on comments.posts_id = posts.id
-      where comments.body  = 'USB';
+      where comments.body like '%USB%';
 
+-- query 11
+select posts.title as post_title, users.first_name, users.last_name, comments.body as comment_body
+  from users
+    inner join posts on posts.users_id = users.id
+      inner join comments on comments.posts_id = posts.id
+        where comments.body like '%matrix%';
 
+-- query 12
+select users.first_name, users.last_name, comments.body as comment_body, posts.content
+  from users
+    inner join posts on posts.users_id = users.id
+      inner join comments on comments.posts_id = posts.id
+        where comments.body like '%SSL%' and posts.content like '%dolorum%';
 
-
-
+select users.first_name as post_author_first_name,
+  users.last_name as post_author_last_name,
+  posts.title as post_title, users.username as comment_author_username,
+  comments.body as comment_body
+    from users
+    inner join posts on posts.users_id = users.id
+      inner join comments on comments.posts_id = posts.id
+        where (comments.body like '%SSL%' or comments.body like '%firewall%')
+         and posts.content like '%nemo%';
